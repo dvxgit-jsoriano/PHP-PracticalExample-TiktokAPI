efd49c34f787925886a1e9b32beba5886a91af08
dvxgit-jsoriano <jsoriano@diavox.net>
2023-08-11 10:29:40 +0800

    scopes added to index and callback

--- callback.php
+++ callback.php
@@ -19,7 +19,7 @@ $data = array(
     'code' => $authorizationCode,
     'redirect_uri' => $redirectUri,
     'grant_type' => 'authorization_code',
-    'scope' => 'user.info.basic, video.list'
+    'scope' => 'user.info.basic, video.list',
 );
 
 // Make the POST request using curl
